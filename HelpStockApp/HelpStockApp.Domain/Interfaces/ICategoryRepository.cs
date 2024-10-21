80f64bb8bf53ce9ec15ea0714f8f0c689f241720
victoricoma <victor.icoma@fatec.sp.gov.br>
2024-10-21 10:23:03 -0300

    Aula1021 - Interfaces de Domain

---
+++ HelpStockApp/HelpStockApp.Domain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using HelpStockApp.Domain.Entities;
+
+namespace HelpStockApp.Domain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetCategories();
+
+        Task<Category> GetById(int? id);
+
+        Task<Category> Create(Category category);
+        Task<Category> Update(Category category);
+        Task<Category> Remove(Category category);
+    }
+}
---
+++ HelpStockApp/HelpStockApp.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using HelpStockApp.Domain.Entities;
+
+namespace HelpStockApp.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+
+        Task<Product> GetById(int id);
+
+        Task<Product> Create(Product product);
+        Task<Product> Update(Product product);
+        Task<Product> Remove(Product product);
+    }
+}
