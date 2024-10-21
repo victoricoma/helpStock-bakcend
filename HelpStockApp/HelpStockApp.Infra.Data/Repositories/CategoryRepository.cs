19b5c55ed6113a89e3b423cc8542f3e9ae70f22e
victoricoma <victor.icoma@fatec.sp.gov.br>
2024-10-21 11:32:39 -0300

    Aula1021 - Repositórios

---
+++ HelpStockApp/HelpStockApp.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using HelpStockApp.Domain.Interfaces;
+using HelpStockApp.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using HelpStockApp.Domain.Entities;
+
+namespace HelpStockApp.Infra.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private ApplicationDbContext _categoryContext;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _categoryContext = context;
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            _categoryContext.Add(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> GetById(int? id)
+        {
+            var category = await _categoryContext.Categories.FindAsync(id);
+            return category;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _categoryContext.Categories.OrderBy(c => c.Id).ToListAsync();
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            _categoryContext.Update(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+            _categoryContext.Remove(category);
+            await _categoryContext.SaveChangesAsync();
+            return category;
+        }
+
+    }
+}
